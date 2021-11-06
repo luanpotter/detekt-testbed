7e58b9bab88de80c0650764c8b034221dd355cab
Luan Nico <luanpotter27@gmail.com>
2021-11-06 15:23:35 -0400

    Example: UselessCallOnNotNull

--- src/main/kotlin/xyz/luan/test/Main.kt
+++ src/main/kotlin/xyz/luan/test/Main.kt
@@ -1,5 +1,10 @@
 package xyz.luan.test
 
 fun main() {
-    println("Hello, world!")
+    val el = setOfNotNull(null, null, "a", null).first()
+    println("Hello, world: $el")
+}
+
+fun <T : Any> setOfNotNull(vararg elements: T?): Set<T> {
+    return listOfNotNull(*elements).toSet()
 }
