22f2dd012f70aa59d4a42d4fc952053cd9d5d59a
Luan Nico <luanpotter27@gmail.com>
2023-03-23 15:43:21 -0400

    Detekt bug

--- src/main/kotlin/xyz/luan/test/Main2.kt
+++ src/main/kotlin/xyz/luan/test/Main2.kt
@@ -1,8 +1,6 @@
 package xyz.luan.test
 
 fun main() {
-    val xyz = "a"
-    val luan = "b"
-    val test = "c"
+    val xyz2 = "a"
     println("Hello, world!")
 }
---
+++ src/main/kotlin/xyz/luan/test/Main3.kt
@@ -0,0 +1,10 @@
+package xyz.luan.test
+
+import java.util.List
+
+fun main() {
+    val java = "a"
+    val util = "b"
+    val a = List()
+    println("Hello, world! ${a}")
+}
